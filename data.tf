f86f1284bca7610d8b9a622faee89eb7ef148aac
patrick <mukumbu.katiwa@boldlink.io>
2022-04-13 11:09:25 +0300

    NOSTORY: excluded network interfaces in example

---
+++ data.tf
@@ -0,0 +1,120 @@
+##########################
+### Get the latest AMI
+##########################
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = [var.ami_owner]
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+  filter {
+    name   = "architecture"
+    values = [var.ami_architecture]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "asg" {
+  statement {
+    sid    = "ECR"
+    effect = "Allow"
+
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Logs"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:Describe*",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+  statement {
+    sid    = "ssmAgent"
+    effect = "Allow"
+
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+  statement {
+    sid    = "CWAgent"
+    effect = "Allow"
+
+    actions = [
+      "cloudwatch:PutMetricData",
+      "ec2:DescribeTags",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "template_cloudinit_config" "config" {
+  gzip          = false
+  base64_encode = true
+
+  # Main cloud-config configuration file.
+  part {
+    filename     = "init.cfg"
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/scripts/init.cfg",
+      {}
+    )
+  }
+  # Base Userdata
+  part {
+    content_type = "text/x-shellscript"
+    content = templatefile("${path.module}/scripts/userdata.sh",
+      {
+        debug = var.debug_script,
+      }
+    )
+  }
+  # Cloudwatch config
+  part {
+    content_type = "text/x-shellscript"
+    content = templatefile("${path.module}/scripts/cwldata.sh",
+      {
+        log_group = join("", aws_cloudwatch_log_group.main.*.name),
+        debug     = var.debug_script,
+      }
+    )
+  }
+  # Additional script
+  part {
+    content_type = "text/x-shellscript"
+    content      = var.extra_script
+  }
+}
---
+++ examples/main.tf
@@ -0,0 +1,164 @@
+locals {
+  name = "boldlink-as"
+}
+
+module "complete" {
+  source = "./../"
+
+  # Autoscaling group
+  name                      = local.name
+  launch_template_name      = local.name
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  availability_zones        = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+
+  initial_lifecycle_hooks = [
+    {
+      name                 = "SampleStartupLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 60
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                 = "SampleTerminationLifeCycleHook"
+      default_result       = "CONTINUE"
+      heartbeat_timeout    = 180
+      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
+      # This could be a rendered data resource
+      notification_metadata = jsonencode({ "Bye" = "EC2" })
+    }
+  ]
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
+
+  # Launch template
+  launch_template_description = "Complete launch template example"
+  update_default_version      = true
+  create_launch_template      = true
+  image_id                    = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  ebs_optimized               = true
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+      }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  capacity_reservation_specification = {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  credit_specification = {
+    cpu_credits = "standard"
+  }
+
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 32
+    instance_metadata_tags      = "enabled"
+  }
+
+  placement = {
+    availability_zone = "eu-west-1b"
+  }
+
+  tag = {
+    Name        = "sample-usage-template"
+    Environment = "dev"
+  }
+
+  # Autoscaling Schedule
+  schedules = {
+    night = {
+      min_size         = 0
+      max_size         = 0
+      desired_capacity = 0
+      recurrence       = "0 18 * * 1-5" # Mon-Fri in the evening
+      time_zone        = "Europe/Rome"
+    }
+
+    morning = {
+      min_size         = 0
+      max_size         = 1
+      desired_capacity = 1
+      recurrence       = "0 7 * * 1-5" # Mon-Fri in the morning
+    }
+
+    go-offline-to-celebrate-new-year = {
+      min_size         = 0
+      max_size         = 0
+      desired_capacity = 0
+      start_time       = "2031-12-31T10:00:00Z" # Should be in the future
+      end_time         = "2032-01-01T16:00:00Z"
+    }
+  }
+  # Target scaling policy schedule based on average CPU load
+  autoscaling_policy = {
+    avg-cpu-policy-greater-than-50 = {
+      policy_type               = "TargetTrackingScaling"
+      estimated_instance_warmup = 1200
+      target_tracking_configuration = {
+        predefined_metric_specification = {
+          predefined_metric_type = "ASGAverageCPUUtilization"
+        }
+        target_value = 50.0
+      }
+    },
+    predictive-scaling = {
+      policy_type = "PredictiveScaling"
+      predictive_scaling_configuration = {
+        mode                         = "ForecastAndScale"
+        scheduling_buffer_time       = 10
+        max_capacity_breach_behavior = "IncreaseMaxCapacity"
+        max_capacity_buffer          = 10
+        metric_specification = {
+          target_value = 32
+          predefined_scaling_metric_specification = {
+            predefined_metric_type = "ASGAverageCPUUtilization"
+            resource_label         = "testLabel"
+          }
+          predefined_load_metric_specification = {
+            predefined_metric_type = "ASGTotalCPUUtilization"
+            resource_label         = "testLabel"
+          }
+        }
+      }
+    }
+  }
+}
